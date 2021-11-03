03a293d4c5a0ae54552aa4d48363a74774317019
zakattack9 <zakattack26111@gmail.com>
2021-11-02 22:47:06 -0700

    finished toolbar

--- app/src/components/ToolBar/ToolBar.tsx
+++ app/src/components/ToolBar/ToolBar.tsx
@@ -1,10 +1,37 @@
+import ToolBarItem from './ToolBarItem';
 import './ToolBar.scss';
 
+interface ToolBarItems {
+  [key: string]: Array<string>
+}
+
+const TOOL_BAR_ITEMS: ToolBarItems = {
+  "Import": [
+    "Package Mapping Config",
+    "Test Environment Config",
+  ],
+  "Export": [
+    "Test Environment Config",
+    "Tests to Python unittests",
+  ],
+  "Manage": [
+    "Uploaded Docker Images",
+    "Uploaded Package Mappings",
+  ],
+  "Help": [
+
+  ],
+}
+
 function ToolBar() {
+  let toolBarItems = Object.entries(TOOL_BAR_ITEMS).map(([name, options]) => (
+    <ToolBarItem name={name} options={options} />
+  ));
+
   return (
-    <div className="ToolBar">
-      Toolbar
-    </div>
+    <nav className="ToolBar">
+      {toolBarItems}
+    </nav>
   );
 }
 
