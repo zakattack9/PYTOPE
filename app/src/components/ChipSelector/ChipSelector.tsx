0f21c7039ad5c403b55d9fd5043154d4a7e168f4
zakattack9 <zakattack26111@gmail.com>
2021-11-13 17:46:30 -0700

    finished filter input and chip selector component layouts

--- app/src/components/ChipSelector/ChipSelector.tsx
+++ app/src/components/ChipSelector/ChipSelector.tsx
@@ -1,5 +1,8 @@
+import { useState, useEffect } from 'react';
 import FilterInput from '../FilterInput/FilterInput';
+import Chip from '../Chip/Chip';
 import { ChipSelectorType } from '../../utils/enums';
+import { ChipType } from '../../utils/enums';
 import './ChipSelector.scss';
 
 interface Props {
@@ -8,13 +11,43 @@ interface Props {
   chips: Array<string>,
 }
 
+interface Chip {
+  name: string,
+  type?: ChipType,
+  placeholder?: string,
+  path: Array<string>
+}
+
 function ChipSelector(props: Props) {
+  const [filter, setFilter] = useState('');
+
+  const handleChange = (input: string) => {
+    setFilter(input)
+  }
+
   return (
     <div className="ChipSelector">
       <div className="ChipSelector__title">{props.title}</div>
       <div className="ChipSelector__type">{props.type}</div>
-      <FilterInput />
-      <div className="ChipSelector__chipWrapper"></div>
+      <FilterInput className="ChipSelector__filterInput" onChange={handleChange} />
+      <div className="ChipSelector__chipWrapper">
+        <Chip name="git" type={ChipType.BASE} />
+        <Chip name="submodule" />
+        <Chip name="--branch" placeholder="<branch>" type={ChipType.ARG} />
+        <Chip name="./a_branch" placeholder="<folder>" type={ChipType.VALUE} />
+        <Chip name="git" type={ChipType.BASE} />
+        <Chip name="submodule" />
+        <Chip name="--branch" placeholder="<branch>" type={ChipType.ARG} />
+        <Chip name="./a_branch" placeholder="<folder>" type={ChipType.VALUE} />
+        <Chip name="git" type={ChipType.BASE} />
+        <Chip name="submodule" />
+        <Chip name="--branch" placeholder="<branch>" type={ChipType.ARG} />
+        <Chip name="./a_branch" placeholder="<folder>" type={ChipType.VALUE} />
+        <Chip name="git" type={ChipType.BASE} />
+        <Chip name="submodule" />
+        <Chip name="--branch" placeholder="<branch>" type={ChipType.ARG} />
+        <Chip name="./a_branch" placeholder="<folder>" type={ChipType.VALUE} />
+      </div>
     </div>
   );
 }
--- app/src/components/FilterInput/FilterInput.tsx
+++ app/src/components/FilterInput/FilterInput.tsx
@@ -1,15 +1,46 @@
+import { useState, useEffect, FormEvent, MouseEvent } from 'react';
 import { SearchIcon, CancelIcon } from '../../utils/svg';
 import './FilterInput.scss';
 
 interface Props {
-  // handleInput:
+  onChange: (input: string) => void,
+  className?: string,
 }
 
 function FilterInput(props: Props) {
+  const [value, setValue] = useState('');
+  const [isEmpty, setIsEmpty] = useState(true);
+
+  const handleChange = (e: FormEvent<HTMLInputElement>) => {
+    setValue(e.currentTarget.value);
+  }
+
+  const handleCancel = (e: MouseEvent<HTMLSpanElement>) => {
+    setValue('');
+    setIsEmpty(true);
+  }
+
+  useEffect(() => {
+    if (value === '') setIsEmpty(true);
+    else setIsEmpty(false);
+    props.onChange(value);
+  }, [value, props]);
+
+  const hideCancelIcon = isEmpty ? '--hide' : '';
+
   return (
-    <div className="FilterInput">
+    <div className={`FilterInput ${props.className || ''}`}>
       <SearchIcon className="FilterInput__searchIcon" />
-      <CancelIcon className="FilterInput__cancelIcon" />
+      <input 
+        className="FilterInput__textInput" 
+        type="text" 
+        placeholder="Filter" 
+        onChange={handleChange} 
+        value={value}
+      />
+      <span className="FilterInput__cancelIconWrapper" onClick={handleCancel}>
+        <CancelIcon className={`FilterInput__cancelIcon${hideCancelIcon}`} />
+      </span>
     </div>
   );
 }
