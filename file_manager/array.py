323410dc0ca1c65092bb34950b2c431cc54176f1
Vishwas Mani <vrmani@asu.edu>
2022-03-02 05:00:32 +0000

    Upload New File

---
+++ file_manager/array.py
@@ -0,0 +1,5 @@
+storage = [["*package_mapping_config", "*file_path_config", "*test_designs_config"], [ "*test_results", "*python_unittests"], []]
+
+
+storage = [['*package_mapping_config', '*file_path_config', '*test_designs_config'], ['*test_results', '*python_unittests'], []]
+storage = [['*package_mapping_config', '*file_path_config', '*test_designs_config'], ['*test_results', '*python_unittests'], []]
