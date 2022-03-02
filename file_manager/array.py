1cad84e477b156bd3c3c8ef2c90b6bdf3fe895d4
Vishwas Mani <vrmani@asu.edu>
2022-03-02 05:01:06 +0000

    Update file_manager/array.py

--- file_manager/array.py
+++ file_manager/array.py
@@ -1,5 +1,1 @@
 storage = [["*package_mapping_config", "*file_path_config", "*test_designs_config"], [ "*test_results", "*python_unittests"], []]
-
-
-storage = [['*package_mapping_config', '*file_path_config', '*test_designs_config'], ['*test_results', '*python_unittests'], []]
-storage = [['*package_mapping_config', '*file_path_config', '*test_designs_config'], ['*test_results', '*python_unittests'], []]
