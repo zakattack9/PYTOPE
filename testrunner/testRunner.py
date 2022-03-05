742d63e99a4a76b9b9e83617ece849acb5cc26af
Hayden Hannappel <poppycop2010@gmail.com>
2022-03-04 20:36:15 -0700

    add output functionality and info on what to change for file manager

--- testrunner/testRunner.py
+++ testrunner/testRunner.py
@@ -5,18 +5,24 @@ import logging
 import os
 import sys
 
-def RunTests(folderName):
+def RunTests(inputFolder, outputFolder):
     #need to add to system path for importer to find
-    sys.path.append(os.getcwd() + "\\" + folderName)
-    for filename in os.listdir(folderName):
-        print(filename)
-        if os.path.isfile(os.getcwd() + "\\" + folderName + "\\" + filename):
-            #removes *.py
-            moduleName = filename.split('.', 1)[0]
-            module = importlib.__import__(moduleName)
-            suite = unittest.TestLoader().loadTestsFromModule(module)
-            unittest.TextTestRunner(verbosity=2).run(suite)
-            #print(sys.modules.keys())
+    sys.path.append(os.getcwd() + "\\" + inputFolder)
+    for filename in os.listdir(inputFolder):
+        fileLocation = os.getcwd() + "\\" + inputFolder + "\\" + filename
+        outputLocation = os.getcwd() + "\\" + outputFolder + "\\" + filename + "_output"
+        if os.path.isfile(fileLocation) and filename.split(".", 1)[1] == "py":
+            #this file open needs to be changed during file manager implementation
+            with open(outputLocation, "w") as outputFile:
+                #removes *.py
+                moduleName = filename.split('.', 1)[0]
+                module = importlib.__import__(moduleName)
+                suite = unittest.TestLoader().loadTestsFromModule(module)
+                #file is written during test run
+                unittest.TextTestRunner(outputFile, verbosity=2).run(suite)
+                print(filename)
 
 if __name__ == '__main__':
-    RunTests("sample_tests")
+    #this is where we would access the file manager to open a folder
+    #can easily be broken down into accessing individual files depending on implementation of file manager
+    RunTests("sample_tests", "sample_output")
