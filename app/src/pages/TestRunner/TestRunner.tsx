fce238c932536597a04beecb98ac523f44e5576a
Hayden Hannappel <poppycop2010@gmail.com>
2022-04-13 14:10:54 -0700

    implemented styling improvements to more closely resemble mockup, should be ready for functionality implementation

--- app/src/pages/TestRunner/TestRunner.tsx
+++ app/src/pages/TestRunner/TestRunner.tsx
@@ -2,14 +2,22 @@ import "./TestRunner.scss";
 import TestGroup, { TestInfo } from "../../components/TestGroup/TestGroup";
 
 function TestRunner() {
-  var temp = new TestInfo("Test git submodule", "error", "> ERROR");
-  var temp2 = new TestInfo("Test git submodule2", "running", "> RUNNING");
+  var temp = new TestInfo(
+    "Test git submodule",
+    "error",
+    "Standard Error (stderr)",
+    "> ERROR"
+  );
+  var temp2 = new TestInfo(
+    "Test git submodule2",
+    "running",
+    "Currently Running",
+    "> RUNNING"
+  );
 
   var exampleArray: TestInfo[] = [temp, temp2];
   var exampleArray2: TestInfo[] = [temp2, temp];
-
   var blockArray: TestInfo[][] = [exampleArray, exampleArray2];
-
   var nameArray: string[] = ["testGroup1", "testGroup2"];
 
   return (
@@ -19,8 +27,6 @@ function TestRunner() {
           <TestGroup testGroupName={nameArray[arrIndex]} tests={arrElement} />
         );
       })}
-      {/* needs mapping here */}
-      {/* <TestGroup testGroupName="example1" tests={exampleArray} /> */}
     </div>
   );
 }
