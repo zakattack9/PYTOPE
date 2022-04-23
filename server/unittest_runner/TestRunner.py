e0089539306599dfd0192476799ea9e98bbdb7dc
John Meyer <jcmeyer6@asu.edu>
2022-04-23 06:04:02 -0700

    Make run_tests() take test_files_dir as a parameter, instead of using UnittestFileWriter
    (test_files_dir is the path to the directory where the unit-test files were written)

--- server/unittest_runner/TestRunner.py
+++ server/unittest_runner/TestRunner.py
@@ -1,37 +1,29 @@
 import contextlib
 import importlib
 import io
-import logging
-import unittest
 import json
+import logging
 import re
+import unittest
 from pathlib import Path
-from unittest_file_writer import UnittestFileWriter
 
 
-def run_tests():
-	p1 = Path('../schemas')  # test_json
-	p2 = Path('../docker')  # dockerfiles
-	p3 = Path('../schemas')  # test_files
-
+def run_tests(test_files_dir: Path):
 	logfile = 'debug_test_log'
 	logging.basicConfig(filename=logfile, level=logging.DEBUG, datefmt='%H:%M:%S')
 	logger = logging.getLogger('test_runner')
-
-	test = UnittestFileWriter.parse_and_write_tests(p1, p2, p3)
-
 	data = {}
 	test_data = []
-	for test_block, file in enumerate(test.test_files, start=1):
+	for test_block, file in enumerate(test_files_dir.iterdir(), start=1):
 		importlib.invalidate_caches()
-		module = importlib.import_module(file.class_name)
+		module = importlib.import_module(file.stem)
 		test_suite = unittest.TestLoader().loadTestsFromModule(module)
 		with io.StringIO() as buf:
 			with contextlib.redirect_stdout(buf):
 				result = unittest.TextTestRunner(verbosity=2, stream=buf).run(test_suite)
 			# print(buf.getvalue())   # emit to front-end or send to file manager
 			test_data = buf.getvalue().split('\n')
-		r = re.compile(".*" + file.class_name)
+		r = re.compile(".*" + file.stem)
 		cleaned_data = list(filter(r.match, test_data))
 		data[test_block] = cleaned_data
 	# for i in test_data:
